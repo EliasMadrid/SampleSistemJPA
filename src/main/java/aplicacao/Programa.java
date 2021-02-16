017c3b5efbe9cdb609ef65efa44ffa5405098812
EliasMadrid <eliasmadrid29@gmail.com>
2021-02-16 20:25:30 +0000

    Primeiro atualizacao Projeto

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,55 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.cursojavaudemy.dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		
+		em.getTransaction().begin();		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("conectado");
+		
+		// --------Comando buscar dados ------------
+		
+		/*
+		Pessoa p = em.find(Pessoa.class,2);
+		System.out.println(p);
+		System.out.println("conectado");
+		em.close();
+		emf.close(); */
+		
+		// --------Comando Deletar dados ------------
+		
+				/*
+		Pessoa p = em.find(Pessoa.class,2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println(p);
+		System.out.println("Deletado");
+		em.close();
+		emf.close(); */
+		
+		
+		
+
+	}
+
+}
